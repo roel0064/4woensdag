dbcf97a8a9489afa053e67c40024838307f3706b
roel0064 <roel0064@hz.nl>
2017-10-31 13:22:06 +0100

    duurt lang

--- index.php
+++ index.php
@@ -52,6 +52,7 @@ foreach ($mergedData as $data) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="Style.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </head>
@@ -189,14 +190,19 @@ foreach ($mergedData as $data) {
 
 <div id="openModal1" class="modalDialog">
 <div>
+
     <a href="#close" title="Close" class="close">X</a>
-    <h2>Modal Box 1</h2>
+    <h2>Voeg project toe</h2>
+    <button type="button" class="btn btn-primary btn-lg pull-right">
+          <span class="glyphicon glyphicon-ok"></span> Voeg toe 
+        </button>
     <form>
     Projectnaam:<br>
     <input type="text" name="Projectnaam"><br>
     Projectinformatie:<br>
     <input type="text" name="Projectinformatie">
   </form>
+ 
 </div>
 </div>
 
@@ -206,9 +212,22 @@ foreach ($mergedData as $data) {
   <div id="openModal2" class="modalDialog">
 <div>
     <a href="#close" title="Close" class="close">X</a>
-    <h2>verwijder project</h2>
-    <p><strong>Box 2</strong></p>
-    <p>yadda yadda</p>
+    <h2>Verwijder Project</h2>
+    <button type="button" class="btn btn-primary btn-lg pull-right">
+          <span class="glyphicon glyphicon-trash"></span> Delete 
+        </button>
+            <?php
+            $query = $db->query("SELECT name FROM projects");
+            echo '<select name="Projecten">';
+            while ($row1 = $query->fetch(PDO::FETCH_ASSOC))
+            {
+                echo '<option value="'.$row1['name'].'">'.$row1['name'].'</option>';
+            }
+            echo '</select>';
+            ?> 
+
+        <br>
+        <br>
 </div>
 </div>
 
@@ -219,8 +238,24 @@ foreach ($mergedData as $data) {
 <div>
     <a href="#close" title="Close" class="close">X</a>
     <h2>Voeg competentie toe</h2>
-    <p><strong>Box 3</strong></p>
-    <p>yadda yadda</p>
+    <button type="button" class="btn btn-primary btn-lg pull-right">
+          <span class="glyphicon glyphicon-ok"></span> Voeg toe 
+        </button>
+    <form>
+    Naam competentie:<br>
+    <input type="text" name="Naam competentie"><br>
+  </form>
+  <p>Voeg toe aan Project</p>
+  <?php
+            $query = $db->query("SELECT name FROM projects");
+            echo '<select name="Projecten">';
+            while ($row1 = $query->fetch(PDO::FETCH_ASSOC))
+            {
+                echo '<option value="'.$row1['name'].'">'.$row1['name'].'</option>';
+            }
+            echo '</select>';
+            ?> 
+
 </div>
 </div>
 
@@ -230,8 +265,21 @@ foreach ($mergedData as $data) {
 <div>
     <a href="#close" title="Close" class="close">X</a>
     <h2>Verwijder competentie</h2>
-    <p><strong>Box 4</strong></p>
-    <p>yadda yadda</p>
+    <button type="button" class="btn btn-primary btn-lg pull-right">
+          <span class="glyphicon glyphicon-trash"></span> Delete 
+        </button>
+            <?php
+            $query = $db->query("SELECT name FROM competencies");
+            echo '<select name="Competenties">';
+            while ($row2 = $query->fetch(PDO::FETCH_ASSOC))
+            {
+                echo '<option value="'.$row2['name'].'">'.$row2['name'].'</option>';
+            }
+            echo '</select>';
+            ?>
+            <br>
+            <br>
+
 </div>
 </div>
 
@@ -240,9 +288,14 @@ foreach ($mergedData as $data) {
   <div id="openModal5" class="modalDialog">
 <div>
     <a href="#close" title="Close" class="close">X</a>
-    <h2>>Voeg student toe</h2>
-    <p><strong>Box 5</strong></p>
-    <p>yadda yadda</p>
+    <h2>Voeg student toe</h2>
+    <button type="button" class="btn btn-primary btn-lg pull-right">
+          <span class="glyphicon glyphicon-ok"></span> Voeg toe 
+        </button>
+    <form>
+    Naam Student:<br>
+    <input type="text" name="Naam student"><br>
+  </form>
 </div>
 </div>
 
@@ -252,8 +305,20 @@ foreach ($mergedData as $data) {
 <div>
     <a href="#close" title="Close" class="close">X</a>
     <h2>Verwijder Student</h2>
-    <p><strong>Box 6</strong></p>
-    <p>yadda yadda</p>
+    <button type="button" class="btn btn-primary btn-lg pull-right">
+          <span class="glyphicon glyphicon-trash" ></span> Delete 
+        </button>
+            <?php
+            $query = $db->query("SELECT name FROM students");
+            echo '<select name="Studenten">';
+            while ($row3 = $query->fetch(PDO::FETCH_ASSOC))
+            {
+                echo '<option value="'.$row3['name'].'">'.$row3['name'].'</option>';
+            }
+            echo '</select>';
+            ?>  
+            <br>
+            <br>
 </div>
 </div>
 
